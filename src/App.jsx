afd079b5d700b6f963bdd981e9a2a04acdf35058
Aman Srivastava <amansrivastava57@gmail.com>
2025-09-03 14:08:59 +0530

    structuring components

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,6 @@
-import headerImage from "./assets/react-core-concepts.png";
 import { CORE_CONCEPTS } from "./data.js";
-const reactDescription = ["Fundamental", "Core", "Crucial"];
-
-function genRandomIndex(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-function CoreConcepts({ image, title, description }) {
-  return (
-    <li>
-      <img src={image} als={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
-
-function Header() {
-  const description = reactDescription[genRandomIndex(2)];
-  return (
-    <header>
-      <img src={headerImage} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
+import Header from "./components/Header.jsx";
+import CoreConcept from "./components/CoreConcept.jsx";
 
 function App() {
   return (
@@ -36,14 +9,14 @@ function App() {
       <main>
         <section id="core-concepts">
           <ul>
-            <CoreConcepts
+            <CoreConcept
               title={CORE_CONCEPTS[0].title}
               description={CORE_CONCEPTS[0].description}
               image={CORE_CONCEPTS[0].image}
             />
-            <CoreConcepts {...CORE_CONCEPTS[1]} />
-            <CoreConcepts {...CORE_CONCEPTS[2]} />
-            <CoreConcepts {...CORE_CONCEPTS[3]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
       </main>
---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import headerImage from "../assets/react-core-concepts.png";
+
+const reactDescription = ["Fundamental", "Core", "Crucial"];
+
+function genRandomIndex(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+export default function Header() {
+  const description = reactDescription[genRandomIndex(2)];
+  return (
+    <header>
+      <img src={headerImage} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
