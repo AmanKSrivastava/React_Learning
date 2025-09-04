c359023d5738fd0c7c11edc8a00e122904a48ad7
Aman Srivastava <amansrivastava57@gmail.com>
2025-09-04 12:48:19 +0530

    useState advance

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
-import { CORE_CONCEPTS } from "./data.js";
+import { CORE_CONCEPTS, EXAMPLES } from "./data.js";
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
 import { useState } from "react";
 
 function App() {
-  const [selectedContent, setSelectedContent] = useState(
-    "Please click a button"
-  );
+  const [selectedTopic, setSelectedTopic] = useState("components");
   function handleSelect(selectedButton) {
-    setSelectedContent(selectedButton);
-    console.log(selectedContent);
+    setSelectedTopic(selectedButton);
   }
-  console.log("App component executing");
+
   return (
     <div>
       <Header />
@@ -39,7 +36,13 @@ function App() {
             <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
             <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
-          {selectedContent}
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,5 +1,4 @@
 export default function TabButton({ children, onSelect }) {
-  console.log("Tab component executing");
   return (
     <li>
       <button onClick={onSelect}>{children}</button>
