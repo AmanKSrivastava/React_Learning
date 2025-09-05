1cfb5ce30aa156381b454616db918187e3c55875
Aman Srivastava <amansrivastava57@gmail.com>
2025-09-05 20:46:48 +0530

    React Fragments

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
   }
 
   return (
-    <div>
+    <>
       <Header />
       <main>
         <section id="core-concepts">
@@ -64,7 +64,7 @@ function App() {
           {tabContent}
         </section>
       </main>
-    </div>
+    </>
   );
 }
 
