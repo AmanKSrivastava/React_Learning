c80329f4545efbc83d26c2d847a58b61eb490d79
Aman Srivastava <amansrivastava57@gmail.com>
2025-09-15 23:01:09 +0530

    Base code added

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,13 @@
-import { useState } from "react";
-import Header from "./components/Header.jsx";
-import UserInput from "./components/UserInput.jsx";
-import DisplayResult from "./components/DisplayResult.jsx";
-import { calculateInvestmentResults } from "./util/investment.js";
-function App() {
-  const [currentValue, setCurrentValue] = useState({
-    initialInvestment: 1000,
-    annualInvestment: 12000,
-    expectedReturn: 6,
-    duration: 10,
-  });
+import AuthInputs from './components/AuthInputs.jsx';
+import Header from './components/Header.jsx';
 
-  const isValidInput = currentValue.duration > 0;
-
-  function handleChangeValue(selectedInputName, newValue) {
-    setCurrentValue((prevValue) => {
-      return {
-        ...prevValue,
-        [selectedInputName]: +newValue,
-      };
-    });
-  }
+export default function App() {
   return (
     <>
       <Header />
-      <UserInput
-        currentValue={currentValue}
-        handleChangeValue={handleChangeValue}
-      />
-      {isValidInput ? (
-        <DisplayResult currentInputValues={currentValue} />
-      ) : (
-        <p className="center">Please enter duration greater than 0</p>
-      )}
+      <main>
+        <AuthInputs />
+      </main>
     </>
   );
 }
-
-export default App;
