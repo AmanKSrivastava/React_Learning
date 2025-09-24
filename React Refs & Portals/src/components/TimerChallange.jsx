bf76ce34254fb73beb7c7970d8e7d0354d1a979a
Aman Srivastava <amansrivastava57@gmail.com>
2025-09-24 19:00:46 +0530

    Added ref

--- React Refs & Portals/src/components/TimerChallange.jsx
+++ React Refs & Portals/src/components/TimerChallange.jsx
@@ -1,15 +1,20 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function TimerChallange({ title, targetTime }) {
   const [timerExpired, setTimerExpired] = useState(false);
   const [timerStarted, setTimerStarted] = useState(false);
+  const timer = useRef();
   function handleStart() {
     setTimerStarted(true);
-    setTimeout(() => {
+    timer.current = setTimeout(() => {
       setTimerExpired(true);
     }, targetTime * 1000);
   }
-  
+
+  function handleStop() {
+    clearInterval(timer.current);
+  }
+
   return (
     <section className="challenge">
       <h2>{title}</h2>
@@ -18,8 +23,8 @@ export default function TimerChallange({ title, targetTime }) {
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
       <p>
-        <button onClick={handleStart}>
-          {timerExpired ? "Stop" : "Start"} Challange
+        <button onClick={timerStarted ? handleStop : handleStart}>
+          {timerStarted ? "Stop" : "Start"} Challange
         </button>
       </p>
       <p className={timerStarted ? "active" : undefined}>
