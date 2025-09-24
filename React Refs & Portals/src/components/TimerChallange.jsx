312bfcd646df04376afc2150f22eb877ca223e9b
Aman Srivastava <amansrivastava57@gmail.com>
2025-09-24 18:36:30 +0530

    adding timer state variables

--- React Refs & Portals/src/components/TimerChallange.jsx
+++ React Refs & Portals/src/components/TimerChallange.jsx
@@ -1,14 +1,30 @@
+import { useState } from "react";
+
 export default function TimerChallange({ title, targetTime }) {
+  const [timerExpired, setTimerExpired] = useState(false);
+  const [timerStarted, setTimerStarted] = useState(false);
+  function handleStart() {
+    setTimerStarted(true);
+    setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTime * 1000);
+  }
+  
   return (
     <section className="challenge">
       <h2>{title}</h2>
+      {timerExpired && <p>You lost!</p>}
       <p className="challange-time">
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
       <p>
-        <button>Start Challange</button>
+        <button onClick={handleStart}>
+          {timerExpired ? "Stop" : "Start"} Challange
+        </button>
       </p>
-      <p className="">Time is running / Timer inactive</p>
+      <p className={timerStarted ? "active" : undefined}>
+        {timerStarted ? "Time is running..." : "Timer inactive"}
+      </p>
     </section>
   );
 }
